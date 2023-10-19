0ebb86d3ff8f763d6d10eeabf728c7e83ba1f940
Praveena Vallela <praveenavallela599@gmail.com>
2023-10-19 13:42:01 +0530

    <initial Commit>

---
+++ Day1/FirstSolution/FirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name);
+            Console.WriteLine($"Welcome {name}!!!");
+            int num1;
+            Console.WriteLine("Please enter a number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > 100)
+                Console.WriteLine("Too big");
+            else
+                Console.WriteLine($"The number {num1} is Okay");
+
+        }
+    }
+}
